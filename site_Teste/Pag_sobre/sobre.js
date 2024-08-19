0f996890c13dba5c3fe86e5e938d384eb2fc1db7
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-08-19 03:08:27 -0300

    terminado por hoje

--- site_Teste/Pag_sobre/sobre.js
+++ site_Teste/Pag_sobre/sobre.js
@@ -67,22 +67,3 @@ function updateBubbles() {
 
 // Start the animation
 updateBubbles();
-
-
-
-document.getElementById('newsForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const title = document.getElementById('newsTitle').value;
-    const content = document.getElementById('newsContent').value;
-
-    const newsItem = document.createElement('div');
-    newsItem.classList.add('news-item');
-    newsItem.innerHTML = `<h2>${title}</h2><p>${content}</p>`;
-
-    const gallery = document.getElementById('newsGallery');
-    gallery.appendChild(newsItem);
-
-    // Limpar o formulário
-    document.getElementById('newsForm').reset();
-});
