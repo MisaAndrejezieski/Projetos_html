c2b828a5e507b3ab91ad7c9f6c338f5348cd2185
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-12-05 19:42:36 -0300

    Update baixar_site.bat

--- baixar_site/baixar_site.bat
+++ baixar_site/baixar_site.bat
@@ -4,21 +4,27 @@ setlocal enabledelayedexpansion
 rem Caminho para o wget.exe dentro da pasta baixar_site
 set WGET_PATH=wget.exe
 
-rem Função para executar o wget com o URL inserido
-:run_wget
-set SITE=%1
+rem Usar um script auxiliar para obter o URL do localStorage
+call :get_site_from_js
+set SITE=%RETURN_SITE%
+
+rem Verificar se o URL foi fornecido
 if "%SITE%"=="" (
     echo URL não fornecido. Por favor, insira um URL.
     pause
     exit /b
 )
+
 rem Executar o wget com o URL inserido
 %WGET_PATH% -r -k -l 7 -p -E -np %SITE%
 pause
 exit /b
 
 rem Abrir a interface gráfica
-mshta "input.html"
+mshta "index.html"
+
+:get_site_from_js
+for /f "tokens=*" %%i in ('mshta "javascript:window.external.setReturnValue(localStorage.getItem(\"site\"));close()"') do set RETURN_SITE=%%i
+exit /b
 
 endlocal
-
