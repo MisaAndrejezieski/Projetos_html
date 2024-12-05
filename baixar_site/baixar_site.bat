66e9dd9b9bc44a96a708e3d2eb92e10c53d06309
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-12-05 19:32:19 -0300

    iniciando projeto

---
+++ baixar_site/baixar_site.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal
+
+rem Caminho para o wget.exe dentro da pasta baixar_site
+set WGET_PATH=wget.exe
+
+rem Função para executar o wget com o URL inserido
+:run_wget
+set SITE=%1
+if "%SITE%"=="" (
+    echo URL não fornecido. Por favor, insira um URL.
+    pause
+    exit /b
+)
+%WGET_PATH% -r -k -l 7 -p -E -np %SITE%
+pause
+exit /b
+
+rem Abrir a interface gráfica
+mshta "input.html"::run_wget
+
+endlocal
