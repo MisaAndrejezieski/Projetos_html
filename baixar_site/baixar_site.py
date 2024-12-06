62fa2d9cd0c944e198e1a7b78887144131458f8b
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-12-06 16:52:57 -0300

    Update baixar_site.py

--- baixar_site/baixar_site.py
+++ baixar_site/baixar_site.py
@@ -1,25 +1,33 @@
 import subprocess
+import os
 
-# Caminho para o wget.exe dentro da pasta baixar_site
-WGET_PATH = 'wget.exe'
-
-# Ler o URL do arquivo site_url.txt
-try:
-    with open('site_url.txt', 'r') as file:
-        SITE = file.readline().strip()
-except FileNotFoundError:
-    print("Arquivo site_url.txt não encontrado.")
-    exit()
-
-# Verificar se o URL foi fornecido
-if not SITE:
-    print("URL não fornecido. Por favor, insira um URL.")
-    exit()
-
-# Executar o wget com o URL inserido
-command = [WGET_PATH, '-r', '-k', '-l', '7', '-p', '-E', '-np', SITE]
-try:
-    subprocess.run(command, check=True)
-except subprocess.CalledProcessError as e:
-    print(f"Erro ao executar o wget: {e}")
+def main():
+    # Caminho para o wget.exe dentro da pasta baixar_site
+    wget_path = 'wget.exe'
     
+    # Verificar se o wget.exe está presente
+    if not os.path.exists(wget_path):
+        print("wget.exe não encontrado no caminho especificado.")
+        return
+    
+    # Ler o URL do arquivo site_url.txt
+    try:
+        with open('site_url.txt', 'r', encoding='utf-8') as file:
+            site = file.readline().strip()
+    except FileNotFoundError:
+        print("Arquivo site_url.txt não encontrado.")
+        return
+    
+    # Verificar se o URL foi fornecido
+    if not site:
+        print("URL não fornecido. Por favor, insira um URL.")
+        return
+    
+    # Executar o wget com o URL inserido
+    try:
+        subprocess.run([wget_path, '-r', '-k', '-l', '7', '-p', '-E', '-np', site], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Erro ao executar o wget: {e}")
+
+if __name__ == "__main__":
+    main()
